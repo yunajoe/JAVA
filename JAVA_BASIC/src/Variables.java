0e2fa6c628566220bb95389ecf51a7eb8ac51dec
yunajoe <86518113+yunajoe@users.noreply.github.com>
2022-08-14 15:42:35 +0900

    변수

--- JAVA_BASIC/src/Variables.java
+++ JAVA_BASIC/src/Variables.java
@@ -12,8 +12,7 @@ public class Variables {
         System.out.println(age);
 
         // java에서는 변수의 자료형을 지정하기 때문에, 자료형 외에 값을 넣어주면 오류가 난다.
-        // int age3 = "Thrity";
-        
+        // int age3 = "Thrity";        
         
 
     }
