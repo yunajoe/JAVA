7530f5f01c815e62e77f13f605877db518087b16
yunajoe <86518113+yunajoe@users.noreply.github.com>
2022-08-14 15:43:17 +0900

    자바시작하기

--- JAVA_BASIC/src/HelloWorld.java
+++ JAVA_BASIC/src/HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("안녕세상아!");
         System.out.print("안녕");
-        System.out.print("세상아!");
-
+        System.out.print("세상아!"); 
     }
 }
